22b97a9fe4e6bf7fa3087361dcaa76c1dae494bd
ahaansekhri <69630587+ahaansekhri@users.noreply.github.com>
2021-12-23 17:19:18 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import * as Speech from 'expo-speech';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+   
+
+render(){
+  return (
+    <View style={styles.container}>
+     
+
+      <TextInput
+            style={styles.input}
+            placeholder="type text to speech here"
+            onChangeText={(text) => {
+              this.setState({ text: text }); 
+            }}
+          />
+           <Text>{this.state.text}</Text>
+
+          
+           <TouchableOpacity style={styles.button} onPress={()=>{
+             var speak = () => {
+              var thingToSay = this.state.text;
+                console.log(thingToSay);
+                Speech.speak(thingToSay);
+                 };
+              
+           }}>
+            <Text style={styles.text}>
+              Go 
+            </Text>
+          </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  input: {
+    borderWidth: 2,
+    width: 200,
+  },
+
+  text:{
+    textAlign: "center",
+    color: "white",
+  },
+
+
+  button:{
+    width:50,
+    height:30,
+    padding: 10,
+    margin: 10,
+    backgroundColor:"blue",
+    borderRadius:10,
+
+  },
+
+});
